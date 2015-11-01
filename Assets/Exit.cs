0f510b81dc0509546c386eadcca397a32cccd7de
Ian <ianketje@gmail.com>
2015-11-01 14:14:00 +0100

    Initial commit

---
+++ Assets/Exit.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeApp : MonoBehaviour {
+	public void ExitApp() {
+		Application.Quit();
+	}
+}
---
+++ Assets/MainScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScript : MonoBehaviour {
+
+	public void ExitApplication() {
+		Application.Quit();
+	}
+}
